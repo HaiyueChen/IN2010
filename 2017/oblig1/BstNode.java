6772b62ed2f99ed7cce0d1d42451c42ea02f2dbe
Haiyue Chen <chenhaiyue2010@hotmail.com>
2018-08-08 22:09:48 +0200

    Changes to be committed: 	new file:   .gitignore 	new file:   Bst.java 	new file:   BstNode.java 	modified:   Node.java 	deleted:    Tree.java

---
+++ 2017/oblig1/BstNode.java
@@ -0,0 +1,9 @@
+class BstNode{
+
+    public BstNode parent, left, right;
+    private String data;
+
+    public BstNode(String s){
+        this.
+    }
+}
--- 2017/oblig1/Node.java
+++ 2017/oblig1/Node.java
@@ -1,4 +1,8 @@
 class Node{
 
-    
+    public Node parent, l_child, r_child;
+    public int key;
+    private String data;
+
+    public Node
 }
