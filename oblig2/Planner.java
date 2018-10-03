7d58be583d6ddee88c8bfd19aedfa98126f08eff
HaiyueChen <chenhaiyue2010@hotmail.com>
2018-10-03 10:19:15 +0200

    started

--- oblig2/Planner.java
+++ oblig2/Planner.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -8,15 +10,17 @@ import com.sun.javafx.runtime.SystemProperties;
 
 class Planner {
 
-    public HashMap<Integer, int[]> dependency_map;
-    public LinkedList<Task> tasks;
+    public HashMap<Integer, int[]> dependency_map = new HashMap<>();
+    public HashMap<Integer, Task> tasks = new HashMap<>();
 
     public Planner(String filename) throws FileNotFoundException {
 
         Scanner s = new Scanner(new File(filename));
+        int task_count = Integer.valueOf(s.nextLine());
         while (s.hasNextLine()) {
             String line = s.nextLine();
             if (!line.equals("")) {
+                //Java did not split the line correctly with just space
                 String[] info = line.split("\\s+");
 
                 int task_id = Integer.valueOf(info[0]);
@@ -25,8 +29,10 @@ class Planner {
                 int task_staff = Integer.valueOf(info[3]);
                 
                 int[] dependencies = null;
+                
                 if (info.length - 4 == 1) {
                     dependencies = new int[] {0};
+
                 } 
                 else {
                     dependencies = new int[info.length - 4];
@@ -34,17 +40,54 @@ class Planner {
                         dependencies[i - 4] = Integer.valueOf(info[i]);
                     }
                 }
-                System.out.println("Task id: " + task_id);
-                System.out.println("Task name: " + task_name);
-                System.out.println("Task time: " + task_time);
-                System.out.println("Task staff: " + task_staff);
-                System.out.print("Dependencies: ");
-                for(int i : dependencies){
-                    System.out.print(i + " ");
+
+                this.dependency_map.put(task_id, dependencies);
+
+                tasks.put(task_id, new Task(task_id, task_name, task_time, task_staff));
+    
+            }
+        }
+        for(Task itt : tasks.values()){
+            int[] current_dependency =  dependency_map.get(itt.id);
+            for(int id : current_dependency){
+                if(id != 0){
+                    Task dependent = tasks.get(id);
+                    itt.add_dependency(tasks.get(id));
+
                 }
             }
         }
+        /*
+        for(Task t : tasks.values()){
+            System.out.println("ID: " + t.id + " name: " + t.name);
+            for(Task d : t.dependencies){
+                System.out.print(d.id + " ");
+            }
+            System.out.println(" \n");
+        }
+        */
 
+    }
+
+    public LinkedList<Task> realizable(){
+        //idealy one would not check the realizability of a plan several times,
+        //therefore there is no need for a wipe procedure
+        /*
+        for(Task t : tasks.values()){
+            t.visit_status = 0;
+        }
+        */
+    
+        LinkedList check = null;
+        for(Task t : tasks.values()){
+            if(t.visit_status == 0){
+                check = t.realizable(new LinkedList<>());
+                if(check != null){
+                    return check;
+                }
+            }
+        }
+        return null;
     }
 
 }
--- oblig2/Task.java
+++ oblig2/Task.java
@@ -7,10 +7,11 @@ class Task {
     public String name;
     public int early;
     public int late;
-    public LinkedList<Task> dependencies;
-    public int cntPred;
+    public LinkedList<Task> dependencies = new LinkedList<>();
+    public int visit_status = 0;
+    //public int cntPred;
 
-    public Task(int id, int time, int staff, String name) {
+    public Task(int id, String name, int time, int staff) {
         this.id = id;
         this.time = time;
         this.staff = staff;
@@ -19,6 +20,26 @@ class Task {
 
     public void add_dependency(Task dependent) {
         this.dependencies.add(dependent);
+    }
+
+    public LinkedList<Task> realizable(LinkedList<Task> current_path){
+        if(this.visit_status == 1){
+            current_path.add(this);
+            return current_path;
+        }
+        else{
+            current_path.add(this);
+            this.visit_status = 1;
+            LinkedList check = null;
+            for(Task t : this.dependencies){
+                check = t.realizable(current_path);
+                if(check != null){
+                    return check;
+                }
+            }
+            this.visit_status = 2;
+            return null;
+        }
     }
 
 }
